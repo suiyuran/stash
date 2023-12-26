c1613701003017df48f0d2c6a9f9ebb7ddcf0d6b
suiyuran <suiyuran@163.com>
2023-12-26 16:36:48 +0800

    init

---
+++ scripts/fix-vvebo-fans.js
@@ -0,0 +1,8 @@
+let url = $request.url;
+if (url.includes("selffans")) {
+  let data = JSON.parse($response.body);
+  let cards = data.cards.filter((card) => card.itemid !== "INTEREST_PEOPLE2");
+  $done({ body: JSON.stringify({ ...data, cards }) });
+} else {
+  $done({});
+}
